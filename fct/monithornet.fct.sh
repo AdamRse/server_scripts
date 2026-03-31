0e2bd7c4838cbabcccf1744e5b32d198e3a5f9b6
AdamRse <bakaarion3@gmail.com>
2026-03-31 17:21:15 +0200

    check bdd

--- fct/monithornet.fct.sh
+++ fct/monithornet.fct.sh
@@ -14,3 +14,20 @@ set_check_globals(){
     debug_ "Vérification des variables globales"
     check_vars_exist "COMMAND_NAME NWD_DB_ADDR NWD_DB_NAME"
 }
+
+check_packages(){
+    ! command -v mysql &> /dev/null && eout "Mysql n'est pas installé, impossible d'accéder à la commande 'mysql'. Veuillez installer mysql."
+}
+
+check_connect_db(){
+    local table="incident"
+    local connect_db
+    connect_db="$(mysql -h "${NWD_DB_ADDR}" -u "${NWD_DB_USER}" -p"${NWD_DB_PASSWD}" "${NWD_DB_NAME}" -e "DESCRIBE ${table};")"
+    if [[ $? = 0 ]]; then
+        debug_ "✅ La table ${table} est accessible."
+        return 0
+    else
+        debug_ "Mysql renvoie une erreur : ${connect_db}"
+        eout "La base de données n'existe pas ou n'est pas accessible avec les variables du .env"
+    fi
+}
--- monithornet.sh
+++ monithornet.sh
@@ -9,11 +9,16 @@ COMMAND_NAME="./${MAIN_SCRIPT_PATH}"
 
 source "${ROOT_DIR}/fct/common/terminal-tools.sh" || exit 1
 source "${ROOT_DIR}/fct/common/common-tools.sh" || exit 1
-source "${ROOT_DIR}/fct/network_data.fct.sh"
+source "${ROOT_DIR}/fct/monithornet.fct.sh"
 source "${ROOT_DIR}/.env" || wout "Aucun .env détecté à la racine, les variables seront appliquées par défaut. Pour personnaliser, plaez un .env à la racine sur le modèle de .env.example"
 
-source "${ROOT_DIR}/src/opt-parser/network_data.sh" || exit 1
+source "${ROOT_DIR}/src/opt-parser/monithornet.parser.sh" || exit 1
 
 
 # -- MAIN
+check_packages
 set_check_globals
+check_connect_db
+
+
+echo "ok"
